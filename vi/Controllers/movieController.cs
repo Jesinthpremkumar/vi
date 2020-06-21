13fd6e952f1a3cd71601f57f9a1a1ebf219951bf
Jesinth PremKumar <jasinth5@gmail.com>
2020-06-21 20:43:36 +0530

    Adding files

---
+++ vi/Controllers/movieController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vi.Models;
+
+namespace vi.Controllers
+{
+    public class movieController : Controller
+    {
+        // GET: movie
+        public ActionResult random()
+        {
+            var movie = new movie() { name="shrek"};
+            return View(movie);
+        }
+       public ActionResult byreleased(int year,int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
---
+++ vi/Models/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace vi.Models
+{
+    public class movie
+    {
+        public int id { set; get; }
+        public string name{ set; get; }
+            
+
+    }
+}
